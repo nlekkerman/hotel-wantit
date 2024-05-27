80b581796bd58088af5dc7b37d3cce0a93c9396b
nikola simic <nlekkerman@gmail.com>
2024-05-27 13:14:37 +0000

    Adjusted code DEBUG = False, for deployment on heroku.

--- hotel/settings.py
+++ hotel/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-578^!$5$%ez*e7aigdu)#i4a8&jkyw&=q(v54js$e7g@cas&md'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-nlekkerman-hotelwantit-u2b30lve2e0.ws-eu114.gitpod.io']
 
