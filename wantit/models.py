477be53c09c984ce8ca3943d168c4f0d29b24a9d
nikola simic <nlekkerman@gmail.com>
2024-05-27 19:16:02 +0000

    Added bookings model.

--- wantit/models.py
+++ wantit/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Booking(models.Model):
+    ROOM_TYPES = [
+        ('deluxe', 'Deluxe Room'),
+        ('superior', 'Superior Room'),
+        ('standard', 'Standard Room'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    checkin_date = models.DateField()
+    checkout_date = models.DateField()
+    type_of_room = models.CharField(max_length=10, choices=ROOM_TYPES)
+    occupied = models.BooleanField(default=False)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return f'{self.name} - {self.checkin_date} to {self.checkout_date}'
