234c22dcb2bbccb35a8f8d283fe0e20409866f38
nikola simic <nlekkerman@gmail.com>
2024-05-27 22:44:09 +0000

    Maintenance Commit

--- hotel/settings.py
+++ hotel/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production, secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-nlekkerman-hotelwantit-u2b30lve2e0.ws-eu114.gitpod.io', '.herokuapp.com']
 
