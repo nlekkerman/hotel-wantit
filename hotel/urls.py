b4ee3ba6bf30ee9d106a5709b367b26d82d92441
nikola simic <nlekkerman@gmail.com>
2024-05-29 11:44:17 +0000

    Bug fixed and review nav link was added.

--- hotel/urls.py
+++ hotel/urls.py
@@ -24,7 +24,7 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path("", include("reviews.urls"), name="reviews"),  # Correct usage of include
+    path("home/", include("reviews.urls")),
     path("", include("home.urls"), name="home"), 
 
 ]
