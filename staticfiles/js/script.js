e40569baf3cd92b45bfd26f3b087d6ded8017eb1
nikola simic <nlekkerman@gmail.com>
2024-06-05 00:14:02 +0000

    Added styles for footer.

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -22,3 +22,37 @@ document.addEventListener('DOMContentLoaded', () => {
     showSlide(currentSlide);
     setInterval(nextSlide, 5000); // Change slide every 3 seconds
 });
+
+
+/*Chat button*/
+document.getElementById('chatButton').addEventListener('click', function() {
+
+  
+    let chatWindow = document.getElementById('chatWindow');
+    if (chatWindow.style.display === 'none' || chatWindow.style.display === '') {
+        chatWindow.style.display = 'flex';
+    } else {
+        chatWindow.style.display = 'none';
+    }
+});
+
+document.getElementById('closeChat').addEventListener('click', function() {
+    var chatWindow = document.getElementById('chatWindow');
+    chatWindow.style.display = 'none';
+});
+
+document.getElementById('sendButton').addEventListener('click', function() {
+    var chatBody = document.querySelector('.chat-body');
+    var chatInput = document.getElementById('chatInput');
+    var newMessage = document.createElement('div');
+    newMessage.classList.add('chat-message');
+    newMessage.innerHTML = `<img src="https://via.placeholder.com/30" alt="avatar" class="avatar">
+                            <div class="message-content">${chatInput.value}</div>`;
+    chatBody.appendChild(newMessage);
+    chatInput.value = '';
+    chatBody.scrollTop = chatBody.scrollHeight; // Scroll to the bottom of the chat body
+});
+
+document.getElementById('toggleButton').addEventListener('click', function() {
+    document.getElementById('navbar').classList.toggle('active');
+});
